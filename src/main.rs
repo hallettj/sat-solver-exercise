8c83ebca28a74cd7c4b0dbb1f2a8815ba5a34011
Jesse Hallett <jesse@sitr.us>
2020-06-01 11:29:49 -0700

    Read a puzzle from stdin and print solution to stdout

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ mod position;
 pub mod puzzle;
 pub mod solver;
 
-fn main() {
-    println!("Hello, world!");
+use crate::puzzle::Puzzle;
+use crate::solver::solve_puzzle;
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    match read_and_solve_puzzle() {
+        Ok(_) => Ok(()),
+        Err(error) => {
+            eprintln!("Error: {}", error);
+            Ok(())
+        }
+    }
+}
+
+fn read_and_solve_puzzle() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    let puzzle = buffer
+        .parse::<Puzzle>()
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+    let solution =
+        solve_puzzle(&puzzle).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+    println!("{}", &solution);
+    Ok(())
 }
